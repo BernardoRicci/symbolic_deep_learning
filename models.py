b4746ad20bf30330e8906b187367407628488597
BernardoRicci <90769942+BernardoRicci@users.noreply.github.com>
2023-06-04 20:00:03 +0200

    Update models.py

--- models.py
+++ models.py
@@ -250,10 +250,9 @@ class GATLayer(MessagePassing):
 class GAT_GN(nn.Module):
     def __init__(self, n_f, edge_index, hidden=300, out_dim=100, num_heads=4, num_layers=3):
         super(GAT_GN, self).__init__()
-
         self.num_layers = num_layers
         self.layers = nn.ModuleList()
-	self.edge_index = edge_index
+        self.edge_index = edge_index
 
         self.layers.append(GATLayer(n_f, hidden, num_heads))
 
